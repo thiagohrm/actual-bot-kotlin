fe4ba2531e5eb99029d0ae3056a94a1030cc3253
Thiago Mattos <thiagohrm@gmail.com>
2026-06-25 10:14:59 -0300

    Add Ktor and SLF4J dependencies to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.11"
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.3.11")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.11")
+    implementation("io.ktor:ktor-client-cio:2.3.11")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.11")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
+    implementation("org.slf4j:slf4j-simple:2.0.12")
+}
