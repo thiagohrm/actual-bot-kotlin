99d436e0a802ebb2cda9ab6c13bbf40ffbe53763
Thiago Mattos <thiagohrm@gmail.com>
2026-06-25 11:16:53 -0300

    Add Shadow plugin for JAR packaging
    Added the Shadow plugin to package dependencies into a single JAR.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.11"
+    id("com.github.johnrengelman.shadow") version "8.1.1" // Adicione esta linha
 }
 
 dependencies {
@@ -11,4 +12,14 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
     implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("com.google.auth:google-auth-library-oauth2-http:1.23.0")
+}
+
+// Garante que o build gere o jar com todas as dependências
+tasks.shadowJar {
+    archiveBaseName.set("actual-bot")
+    archiveClassifier.set("all")
+    archiveVersion.set("")
+    manifest {
+        attributes["Main-Class"] = "MainKt" // Verifique se o seu arquivo é Main.kt
+    }
 }
