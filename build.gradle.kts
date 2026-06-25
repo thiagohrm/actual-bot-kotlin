11fb5628ea0e9e8bde2520778f2a4c84083c5de2
Thiago Mattos <thiagohrm@gmail.com>
2026-06-25 10:44:21 -0300

    Add Google Auth library dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:2.3.11")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
     implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.23.0")
 }
